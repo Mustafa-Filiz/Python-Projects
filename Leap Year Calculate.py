71391f03c2af985c1429d537d1690b335410fecf
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-04-24 01:49:20 +0300

    leap year corrected

--- Leap Year Calculate.py
+++ Leap Year Calculate.py
@@ -1,10 +1,15 @@
 year = int(input("Enter the year that you want to learn \
 if it's a leap year. : "))
-a = year % 4  
-b = year % 100  
-c = year % 400  
-d = bool(not a or not c and b)
 
-print("The answer is", d, sep=" : ")
+if (year % 4) == 0:
+  leap = True
+  if (year % 100) == 0:
+    leap = False
+    if (year % 400) == 0:
+      leap = True
+else:
+  leap = False
+
+print(leap)
 
 print("True means it'a leap year, False means it's not a leap year.")
